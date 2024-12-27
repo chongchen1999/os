41550bc69e699a1e090f93896e3a9b797a14fb4c
Chong Chen <chongchen1999@gmail.com>
2024-12-27 16:58:43 -0500

    update process

---
+++ 11-process/fork-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    setbuf(stdout, NULL);
+    int n = 2;
+    for (int i = 0; i < n; i++) {
+        fork();
+        printf("Hello\n");
+    }
+    for (int i = 0; i < n; i++) {
+        wait(NULL);
+    }
+}
