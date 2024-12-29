9850830f824af5cf0fe457c1112238b805d3a710
Chong Chen <chongchen1999@gmail.com>
2024-12-29 17:01:02 -0500

    update files

--- 14-c_stdlib/sizeof_.c
+++ 14-c_stdlib/sizeof_.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <stdint.h>
 
 int main() {
     printf("Size of int is %lu bytes\n", sizeof(int));
     printf("Size of long is %lu bytes\n", sizeof(long));
+
+    int64_t a = 10;
+    printf("Size of a is %lu bytes\n", sizeof(a));
+    printf("Value of a is %ld\n", a);
+
     return 0;
 }
---
+++ 14-c_stdlib/sys_.c
@@ -0,0 +1,30 @@
+#include <errno.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+extern char **environ;  // Access to the process's environment variables
+
+int main() {
+    char *argv[] = {"/bin/echo", "hello", "world",
+                    NULL};  // Arguments for the echo command
+
+    if (fork() == 0) {
+        printf("Child process\n");
+        printf("PID: %d\n", getpid());
+        fflush(stdout);  // Flush the output buffer before calling execve
+        if (execve(argv[0], argv, environ) < 0) {
+            perror("execve");  // Print an error message if execve fails
+            return 1;          // Return a non-zero exit code on failure
+        }
+    }
+
+    printf("Parent process\n");
+    printf("PID: %d\n", getpid());
+    fflush(stdout);
+    execlp("echo", "echo", "hello", "world", NULL);
+    // system("echo hello world");
+
+    return 0;  // This will never be reached if execve succeeds
+}
